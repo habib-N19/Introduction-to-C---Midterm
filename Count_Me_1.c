383cd7ca3e47f2274fa88f62e2454dde6d97cd72
Habibur <habiburnabiarafat@gmail.com>
2024-04-27 14:53:59 +0600

    problem 1 solved

--- Count_Me_1.c
+++ Count_Me_1.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     // Your code here
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    // printf("%d", a);
+    // printf("hello\n");
+    // int cnt[2];
+    int dv2 = 0;
+    int dv3 = 0;
+    for (int i = 0; i < n; i++)
+
+    {
+        int value = a[i];
+        // printf("%d", value);
+        if (value % 2 == 0 && value % 3 == 0)
+        {
+            dv2++;
+        }
+        else if (value % 2 == 0 && value % 3 != 0)
+        {
+            dv2++;
+        }
+        else if (value % 2 != 0 && value % 3 == 0)
+        {
+            dv3++;
+        };
+    }
+    printf("%d %d", dv2 , dv3);
 
     return 0;
 }
